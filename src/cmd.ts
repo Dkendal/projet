c3eec41f06978a7d6266cfccf84192e3dba85cfd
Dylan Kendal <dylankendal@gmail.com>
2021-01-25 14:05:52 -0500

    Remove unused file

--- src/cmd.ts
+++
@@ -1,6 +0,0 @@
-import * as projet from './projet'
-
-export async function link(file: string, linkName?: string) {
-  const config = await projet.getConfig(file)
-  return projet.assoc(config, file, linkName)
-}
